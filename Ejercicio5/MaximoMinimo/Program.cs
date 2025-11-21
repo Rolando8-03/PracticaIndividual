a25c92f85b176cea9eea05c42ad35c750520e0f2
Rolando8 <remena@uamv.edu.ni>
2025-11-20 22:20:15 -0600

    Ejercicio 5 realizado con éxito

---
+++ Ejercicio5/MaximoMinimo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximoMinimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var aleatorio = new Random();
+            List<int> lista = new List<int>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                lista.Add(aleatorio.Next(1, 100));
+            }
+
+            Console.WriteLine("Lista de números:");
+            foreach (var n in lista) Console.Write(n + " ");
+
+            int maximo = lista[0];
+            int minimo = lista[0];
+            int iteraciones = 0;
+
+            foreach (var n in lista)
+            {
+                iteraciones++;
+
+                if (n > maximo) maximo = n;
+                if (n < minimo) minimo = n;
+            }
+
+            Console.WriteLine($"\nMáximo: {maximo}");
+            Console.WriteLine($"Mínimo: {minimo}");
+            Console.WriteLine($"Iteraciones: {iteraciones}");
+        }
+    }
+}
